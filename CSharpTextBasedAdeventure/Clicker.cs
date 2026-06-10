cde1ee45f25198b9702ae320936af3099a1fd9a6
Y.Ouallal <2235702@talnet.nl>
2026-06-10 11:29:59 +0200

    Shop Added and More
    The shop has been added, which means that the player can buy upgrades to earn even more cookies. The amount of money you receive after selling cookies has also been changed.

--- CSharpTextBasedAdeventure/Clicker.cs
+++ CSharpTextBasedAdeventure/Clicker.cs
@@ -1,32 +1,28 @@
-﻿using CSharpTextBasedAdeventure;
+﻿using CSharpTextBasedAdventure;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace CSharpTextBasedAdventure
 {
     internal class Clicker
     {
-        private int _amountCookies;
-        private int _amountMoney;
+        private float _amountCookies;
+        private float _amountMoney;
 
-        private List<Upgrades> _upgrades = new List<Upgrades>();
-
-        public void PlayerInfo()
+        private List<Upgrades> _upgrades = new List<Upgrades>()
         {
-            Console.WriteLine($"Cookies: {_amountCookies}");
-            Console.WriteLine($"Money: ${_amountMoney}");
-            Console.WriteLine("Druk op spatie voor een cookie.");
-            Console.WriteLine("Typ /sell om je cookies te verkopen voor geld.");
-        }
+            new Grandma(),
+            new Factory()
+        };
 
         public void Start()
         {
             while (true)
             {
-                Console.Clear();
                 PlayerInfo();
 
                 string input = Console.ReadLine();
@@ -41,8 +37,26 @@ namespace CSharpTextBasedAdventure
                 }
                 else if (input == "/shop")
                 {
-                    //Shop();
+                    Shop();
+
+                    int choice;
+
+                    while (!int.TryParse(Console.ReadLine(), out choice) || choice < 0 || choice > _upgrades.Count)
+                    {
+                        Shop();
+                        Console.WriteLine("\nOngeldig keuze. Typ opnieuw in.");
+                    }
+
+                    if (choice == 0)
+                    {
+                        Console.Clear();
+                        continue;
+                    }
+
+                    BuyUpgrade(choice);
                 }
+
+                Console.Clear();
             }
         }
 
@@ -54,32 +68,75 @@ namespace CSharpTextBasedAdventure
 
                 foreach (Upgrades upgrade in _upgrades)
                 {
-                    totalCps += upgrade.CookiesPerSecond;
+                    totalCps += upgrade.CookiesPerSecond * upgrade.AmountOwned;
                 }
 
                 _amountCookies += totalCps;
-
-                Console.Clear();
-                PlayerInfo();
 
                 await Task.Delay(1000); // wacht 1 seconde
             }
         }
 
-        public void BuyGrandma()
+        public void Shop()
         {
-            _upgrades.Add(new Grandma());            
+            Console.Clear();
+            Console.WriteLine($"Money: $ {_amountMoney}");
+            Console.WriteLine("\n=== SHOP ===");
+
+            for (int i = 0; i < _upgrades.Count; i++)
+            {
+                Upgrades u = _upgrades[i];
+                Console.WriteLine($"{i + 1}. {u.Name} - $ {u.Cost}");
+            }
+
+            Console.WriteLine("\n0. Terug");
+        }
+
+        public void PlayerInfo()
+        {
+            Console.WriteLine($"Cookies: {_amountCookies}");
+            Console.WriteLine($"Money: ${_amountMoney}");
+            Console.WriteLine("Druk op spatie en dan op enter voor een cookie.");
+            Console.WriteLine("Typ /sell om je cookies te verkopen voor geld.");
+            Console.WriteLine("Typ /shop om naar de shop te gaan.");
+        }
+
+        public void BuyUpgrade(int index)
+        {
+            Upgrades choice = _upgrades[index - 1];
+
+            if (_amountMoney >= choice.Cost)
+            {
+                _amountMoney -= choice.Cost;
+                choice.AmountOwned++;
+
+                choice.Cost = (int)(choice.Cost * 1.2);
+                
+                Console.WriteLine($"\nJe hebt een {choice.Name} gekocht!");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine($"\nJe hebt niet genoeg cookies.");
+                Console.ReadKey();
+            }
         }
 
         private void Click()
         {
             _amountCookies += 1;
+
+            Console.Clear();
+            PlayerInfo();
         }
 
         private void Sell()
         {
-            _amountMoney += _amountCookies;
+            _amountMoney += _amountCookies * 1.75f;
             _amountCookies = 0;
+
+            Console.Clear();
+            PlayerInfo();
         }
     }
 }
--- CSharpTextBasedAdeventure/Program.cs
+++ CSharpTextBasedAdeventure/Program.cs
@@ -1,5 +1,4 @@
-﻿using CSharpTextBasedAdeventure;
-using CSharpTextBasedAdventure;
+﻿using CSharpTextBasedAdventure;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,15 +11,6 @@ namespace CSharpTextBasedAdventure
     {
         static void Main(string[] args)
         {
-            List<Upgrades> upgrades = new List<Upgrades>();
-            upgrades.Add(new Grandma());
-            upgrades.Add(new Factory());
-
-            foreach (Upgrades u in upgrades)
-            {
-                u.Info();
-            }
-
             Clicker game = new Clicker();
 
             _ = game.GenerateCookies();
